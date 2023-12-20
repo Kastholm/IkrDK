9a9d9a1b83ff4344b0d6609dcdcd5c610d95d83d
Anton B. Christensen <webbermanden@gmail.com>
2023-12-20 18:47:58 +0100

    flere respons ændringer

--- components/ForsideBund.vue
+++ components/ForsideBund.vue
@@ -2,18 +2,18 @@
 <main class=" overflow-x-hidden">
   <section class="w-full flex justify-center mt-40 max-h-[390px] relative">
     <div class="bgimg flex flex-col sm:flex-row justify-around items-center max-w-full sm:max-w-full">
-        <div class="flex flex-col mb-20 sm:ml-20 sm:mt-0 gap-10 sm:text-start text-center sm:-translate-y-5 ">
+        <div class="flex flex-col mb-20 sm:ml-20 sm:mt-0 gap-10 sm:text-start text-center sm:-translate-y-16 2xl:-translate-y-2">
         <h4> Tæt på det hele</h4>
         <p class="w-[90%] m-auto sm:m-0 ikrfont max-w-[700px] font-semibold"> Som en del af godset Hørbygård i Nordvestsjælland har IKR udgangspunkt fra det gamle kampestenshus. Midt i den skønneste natur, nær skov og fjord. 5 minutter til motorvejen til København og 30 minutter til færgen til Århus. Tæt på det hele – også vores kunder.</p>
         </div>
-        <img src="https://i.ibb.co/db0jP2z/IKR-0009.jpg" class="sm:max-w-[500px] max-h-[400px] sm:mr-16 rounded-sm mt-10 sm:mt-0  -translate-y-12 sm:-translate-y-14 " alt="">
+        <img src="https://i.ibb.co/db0jP2z/IKR-0009.jpg" class="sm:max-w-[500px] max-h-[400px] sm:mr-16 rounded-sm mt-10 sm:mt-0  -translate-y-12 sm:-translate-y-24 2xl:-translate-y-10 " alt="">
     </div>
     
   </section>
 
-  <section class="flex justify-center w-full relative sm:mt-0 mt-72">
-<figure class="h-[200px] min-h-[200px] w-[180px] bg-[#2c2c2c] clip3 absolute left-0 top-0 -translate-y-36 sm:-translate-y-5"></figure>
- <div class="flex flex-col mt-20 items-center gap-5 w-[60%] sm:text-start text-center">
+  <section class="flex justify-center w-full relative sm:mt-20 mt-72">
+<figure class="h-[200px] min-h-[200px] w-[180px] bg-[#2c2c2c] clip3 absolute left-0 top-0 -translate-y-36 sm:-translate-y-5 "></figure>
+ <div class="flex flex-col mt-20 items-center gap-5 w-[60%] sm:text-start text-center md:mt-0 2xl:mt-72">
     <h2>🙷 IKRs assistance har givet os skarpere performance og compliance.. 🙷</h2>
     <p> Læs hvordan nogle af vores kunder har vurderet samarbejdet og de resultater processen har givet.</p>
     <NuxtLink to="#" class="font-semibold underline underline-offset-2"> Se mere her </NuxtLink>
@@ -33,7 +33,8 @@
 }
 .bgimg {
     background-image: url('https://i.ibb.co/jTpr9xR/IKR-gr-n-baggrund-4000x1000px-1.jpg');
-    height:500px;
+    height:560px;
+    
     width:100%;
     background-size: contain;
     background-repeat: no-repeat;
--- components/ForsideHero.vue
+++ components/ForsideHero.vue
@@ -2,7 +2,7 @@
 <main>
  <div class="relative h-[800px] mt-0 z-10 header-img max-w-[2000px] w-full m-auto">
    <div>
-    <img src="https://i.ibb.co/MGXRBFt/FORSIDE-h-nd.png" class=" handrespons left-[325px] top-0 h-[500px] scale-[2.1] absolute -translate-y-16 z-20" alt="">
+    <img src="https://i.ibb.co/MGXRBFt/FORSIDE-h-nd.png" class=" grid place-content-center grid-flow-row handrespons handrespons2 left-[325px] top-0 h-[500px] scale-[2.1] absolute -translate-y-16 z-20" alt="">
    </div>
    <div class="w-full h-[400px] bg-[#256c2b] absolute bottom-0 ">
     <div class=" bg-[#000000] w-[10%] h-[600px] top-0 absolute z-30 opacity-20 clip handrespons">
@@ -38,6 +38,20 @@ export default {
     .handrespons {
         display: none;
     }
+}
+
+@media screen and (min-width: 1600px) {
+  
+  .handrespons2  {
+    transform: translatex(20px) scale(2.1);
+  }
+
+  .container {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  /* Add more styles as needed */
 }
  
 .clip {
