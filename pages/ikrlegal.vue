70ca173360c8b19a0941be0bccd8092cd52719f1
Anton Christensen <antonchristensen@Antons-MacBook-Pro.local>
2024-02-27 12:06:22 +0100

    legal privacy underside indsat

--- pages/ikrlegal.vue
+++ pages/ikrlegal.vue
@@ -5,7 +5,7 @@
      <h1 class="text-black mt-40"><span class="text-[#256c2b]">IKR</span> legal</h1>
 <ul class="text-center mt-10 flex flex-col gap-2 text-[#256c2b] border-r-4 border-l-4 px-3 border-[#256c2b]">
     <NuxtLink to="legalwebsite"> <li class="hover:underline">Website Terms of Use</li> </NuxtLink>
-    <NuxtLink to="#"> <li class="hover:underline">Privacy and Cookie Policy</li> </NuxtLink>
+    <NuxtLink to="legalprivacy"> <li class="hover:underline">Privacy and Cookie Policy</li> </NuxtLink>
     <NuxtLink to="#"> <li class="hover:underline">Data Processing Agreement</li> </NuxtLink>
     <NuxtLink to="#"> <li class="hover:underline">Data Sub-Processors</li> </NuxtLink>
     <NuxtLink to="#"> <li class="hover:underline">GDPR Commitment</li> </NuxtLink>
