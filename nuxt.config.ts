7ecd8ee6ce48859a2dc5cafae10c5f3ed564ef81
Anton B. Christensen <webbermanden@gmail.com>
2024-04-18 16:11:47 +0200

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
-
-export default defineNuxtConfig({
+export default {
   devtools: { enabled: true },
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
     }
   },
   postcss: {
@@ -23,4 +27,4 @@ export default defineNuxtConfig({
   i18n: {
     vueI18n: './i18n.config.ts' // if you are using custom path, default
   }
-});
+}
