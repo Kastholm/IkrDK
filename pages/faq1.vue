fe5c61266e3050c85545dc722f26e372b0200392
Anton B. Christensen <webbermanden@gmail.com>
2024-08-21 16:51:43 +0200

    FAQ undersider indsat klar til duplikering

---
+++ pages/faq1.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-20 lg:mt-40 m-auto max-w-[90%] lg:max-w-[85%]">
+   <a class="inline-block w-fit mb-5" href="faq.vue">
+    <div class="flex gap-3 w-fit">
+     <img class="" style="height: 40px; width: auto;" src="https://i.ibb.co/hBhJQQh/vecteezy-arrow-to-the-left-icon-sign-14455839.png">
+     <p class="inline font-bold translate-y-[2px]">Tilbage</p>
+    </div>
+   </a>
+  <h1 class="mb-7">FAQ spørgsmål 1 overskrift</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis odio blandit pharetra porta. Nulla consectetur mi et rhoncus ultricies. Nulla rutrum, dui non dignissim accumsan, diam justo ullamcorper sapien, quis mollis justo tortor id urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce porttitor ut nunc vel fermentum. Integer blandit tincidunt nunc, nec bibendum ligula fringilla et. Pellentesque faucibus vestibulum ullamcorper. Aliquam suscipit blandit tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+Suspendisse lobortis lorem eget ipsum efficitur, vel ullamcorper libero gravida. Aliquam erat volutpat. Phasellus tincidunt enim at elit finibus ultrices. Curabitur nec nunc ullamcorper, posuere dui in, congue risus. Pellentesque in sapien nunc. Quisque interdum volutpat aliquam. Donec pulvinar magna ac tortor hendrerit, eget sodales tortor commodo. Ut lectus mauris, scelerisque in eros eu, auctor porttitor leo.
+
+Vestibulum tincidunt sem massa, volutpat commodo purus accumsan at. Aliquam sed turpis in justo aliquam cursus tincidunt in nunc. Morbi fringilla mauris sed mattis pharetra. Suspendisse cursus, urna quis ornare accumsan, neque dolor scelerisque velit, quis scelerisque leo leo eget velit. Morbi hendrerit tellus mi, quis eleifend leo fermentum in. Vivamus et vestibulum urna. Vestibulum orci mauris, ultricies id placerat non, iaculis ultricies erat. Sed erat tortor, consectetur et ligula ut, lacinia consequat mi. Cras risus erat, suscipit laoreet nunc et, consequat aliquam turpis. Nam eu nunc eget risus tempus posuere. Sed rutrum nisi ut erat scelerisque, non vulputate diam gravida.</p>
+<h2 class="mt-7 mb-7">Mellemsektion</h2>
+<p>
+Vestibulum efficitur elit enim, dignissim pharetra erat porttitor a. Curabitur vitae nibh at nunc interdum venenatis. Pellentesque suscipit, enim quis sodales aliquet, metus dui suscipit risus, vulputate blandit enim nulla quis metus. Pellentesque mollis cursus accumsan. Ut vulputate mi nec mauris porta venenatis. Ut mi nisl, congue vitae elementum vel, volutpat eget tellus. Sed luctus quam sit amet purus vulputate, convallis facilisis enim ultrices. Aenean non sodales nisl. Praesent ullamcorper augue eget tortor hendrerit, sed dignissim libero venenatis. Sed tincidunt urna quis feugiat pulvinar.
+
+Aenean sit amet velit ac velit facilisis lobortis. Maecenas tristique ullamcorper velit ac malesuada. Nulla lobortis justo ipsum, at iaculis sapien malesuada et. Fusce gravida sollicitudin leo, vel rutrum nunc. In pharetra felis sed risus laoreet mattis. Mauris eget leo arcu. Morbi vulputate, lacus et tempor tempor, metus orci consequat tortor, a mollis massa risus vel metus. Integer molestie lorem leo, ut aliquam arcu placerat vel. Suspendisse ac nibh vitae odio iaculis maximus sed eget nulla. Fusce elementum hendrerit lacinia. Nam tempor tincidunt metus at interdum. Fusce lacinia lectus scelerisque leo dictum, ornare commodo ipsum ullamcorper. Quisque condimentum ex vel nibh tincidunt ultricies ac at augue.
+</p>
+<a class="inline-block w-fit mt-10" href="faq.vue">
+    <div class="flex gap-3 w-fit">
+     <img class="" style="height: 40px; width: auto;" src="https://i.ibb.co/hBhJQQh/vecteezy-arrow-to-the-left-icon-sign-14455839.png">
+     <p class="inline font-bold translate-y-[2px]">Tilbage</p>
+    </div>
+   </a>
+</div>
+</template>
+
+<script setup>
+useSeoMeta({
+  title: 'FAQ1 titel',
+  ogTitle: 'FAQ1 SoMe link titel',
+  description: 'FAQ1 meta tag',
+  ogDescription: 'FAQ1 SoMe meta tag',
+  ogImage: 'https://i.ibb.co/ZWCZJRk/IKR-Main-Logo-600x450-cropped.jpg',
+})
+</script>
+
+<style>
+
+</style>
